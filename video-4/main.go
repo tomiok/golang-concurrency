ee957ba28fcafe860c7177f1b3c001ca947cad26
tomaslingotti <tomaslingotti@gmail.com>
2020-05-26 21:17:07 -0300

    video 4

--- video-4/main.go
+++ video-4/main.go
@@ -1,24 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-	sender := make(chan string, 1)
-	receiver := make(chan string, 1)
 
-	send(sender, "hola")
-
-	receiveAndSend(sender, receiver)
-	msg := <-receiver
-
-	fmt.Println(msg)
-}
-
-func send(c chan<- string, msg string) {
-	c <- msg
-}
-
-func receiveAndSend(r <-chan string, s chan<- string) {
-	msg := <-r
-	s <- msg
 }
